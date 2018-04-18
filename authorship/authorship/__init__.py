07ddfa24679758c0f57c976a0a4edb04100e5ac8
luto <m@luto.at>
2018-04-18 15:58:51 +0200

    list and link all guides an author has contributed to in hall of fame

--- authorship/authorship/__init__.py
+++ authorship/authorship/__init__.py
@@ -2,19 +2,21 @@ import itertools
 
 from docutils import nodes
 from sphinx.util.docutils import SphinxDirective
+import sphinx.addnodes as addnodes
 from docutils.parsers.rst import directives
 
 
-def bullet_list(items):
-    lst = nodes.bullet_list()
+def comma_list(nodes_):
+    elements = []
 
-    for item in items:
-        lst_item = nodes.list_item()
-        lst += lst_item
-        lst_item += nodes.paragraph(text=item)
+    if not nodes_:
+        return []
 
-    return lst
+    for node in nodes_:
+        elements.append(node)
+        elements.append(nodes.Text(', '))
 
+    return elements[:-1]
 
 class Author(SphinxDirective):
     required_arguments = 1
@@ -37,13 +39,7 @@ class Authors(SphinxDirective):
         if not authors:
             return []
 
-        author_elems = []
-
-        for author in authors:
-            author_elems.append(nodes.Text(author))
-            author_elems.append(nodes.Text(', '))
-
-        return [nodes.Text('Written by: ')] + author_elems[:-1]
+        return [nodes.Text('Written by: ')] + comma_list(nodes.Text(a) for a in authors)
 
 # maker node later to be replaced by list of all authors
 class allauthors(nodes.General, nodes.Element):
@@ -69,9 +65,37 @@ def purge_authors(app, env, docname):
 def process_authorlists(app, doctree, fromdocname):
     env = app.builder.env
     authors = set(itertools.chain(*[authors for authors in env.authors.values()]))
+    guides_by_author = {
+        a: set(g for g, guide_authors in env.authors.items() if a in guide_authors)
+        for a in authors
+    }
 
     for node in doctree.traverse(allauthors):
-        node.replace_self([bullet_list(authors)])
+        lst = nodes.bullet_list()
+
+        for author in authors:
+            lst_item = nodes.list_item()
+            lst += lst_item
+            lst_item += addnodes.compact_paragraph(text=author)
+
+            lst_item += nodes.raw('', '<br>', format='html')
+
+            links = []
+
+            for guide in guides_by_author[author]:
+                # I can't figure out a way to get the link and title from a page name..
+                link = guide + '.html'
+                title = guide.partition('_')[2].title()
+
+                link_wrapper = addnodes.compact_paragraph()
+                link_wrapper += nodes.reference('', '', nodes.Text(title), internal=True, refuri=link, anchorname='')
+
+                links.append(link_wrapper)
+
+            for n in comma_list(links):
+                lst_item += n
+
+        node.replace_self([lst])
 
 
 def setup(app):
