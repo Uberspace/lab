bc8005ea62567cf5b8b3b08ae572d55ec4f8b3f3
Janto <janto@team.uber.space>
2018-05-07 17:37:44 +0200

    :bento: format author lines in Hall of Fame

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -21,8 +21,47 @@ $(function () {
     }
   }
 
+  function replaceEntities (text) {
+    text = text.replace('&lt;', '<')
+    text = text.replace('&gt;', '>')
+    return text
+  }
+
+  function formatExtra (text) {
+    text = replaceEntities(text)
+    // TODO: auto–links
+    return text
+  }
+
+  function formatAuthor (ele) {
+    const text = ele.html().trim()
+    const idx = text.search(/[^\w\s]/)
+    const author = $('<span class="author"></span>')
+    const extra = $('<small></small>')
+    ele.html('')
+    if (idx >= 0) {
+      author.text(text.slice(0, idx).trim())
+      extra.text(formatExtra(text.slice(idx).trim()))
+    } else {
+      author.text(text.trim())
+    }
+    ele.append(author, ' ', extra)
+  }
+
+  // Auf gehts…
+
   console.log('UberLab launched…')
 
   // fix spacing between border and text
   checkLogoHeight()
+
+  // "hall of fame" stuff…
+  if ($('#hall-of-fame').length) {
+    console.log('Welcome to the "Hall of Fame"!')
+    // format author lines
+    const authors = '#hall-of-fame > ol > li > div:nth-child(2)'
+    $(authors).each(function () {
+      formatAuthor($(this))
+    })
+  }
 })
