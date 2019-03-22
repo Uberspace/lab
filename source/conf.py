5b1ec9d1dc7f22bfecc7226fbc1d300f5e704bb3
luto <m@luto.at>
2019-03-22 14:43:42 +0100

    redirect default netlify domain to lab.uberspace.de

--- source/conf.py
+++ source/conf.py
@@ -135,6 +135,7 @@ html_favicon = '_static/favicon.ico'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ['_redirects']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
