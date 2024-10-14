a2bf3a544070781f105ce217fec824b6a1a80ac3
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2024-10-14 14:23:32 +0200

    Set language to a defined value, None is no longer supported

--- source/conf.py
+++ source/conf.py
@@ -71,7 +71,7 @@ release = version = "7"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en_GB"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
